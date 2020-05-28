ba8053d7c98913429c10cda951c6ef81b7e2654e
Gabriel Reis <reisrgabriel@gmail.com>
2020-05-28 17:39:09 -0300

    Update Part 3
    Finalizadas as análises de rede tanto com ACSF/ACP escore geral quanto com ACSF/ACP escore dos fatores.

--- Análises/Part 3 - Network Analysis.R
+++ Análises/Part 3 - Network Analysis.R
@@ -7,17 +7,19 @@
 # Banco de dados completo
     network <- read.csv("https://raw.githubusercontent.com/GabrielReisR/CarreirasContemporaneas/master/Bancos/network%20analysis.csv", sep = ",")
     # Cada análise terá seu próprio banco, o banco acima contém todos escores desse arquivo
-'''Criando rede 1 - escores específicos
-    AEO 
-    + MP (ACSF) + MF (ACSF) 
-    + AG (ACP) + DV (ACP) 
-    + AUTEN + BALAN + CRESC
-    + EEC
-    '''
-    # lendo banco específico
-    networkEsp <- read.csv
+
+# Criando rede 1 - escores específicos
+ #   AEO 
+ #   + MP (ACSF) + MF (ACSF) 
+ #   + AG (ACP) + DV (ACP) 
+ #   + AUTEN + BALAN + CRESC
+ #   + EEC
+
+ # lendo banco específico
+    networkEsp <- read.csv("https://raw.githubusercontent.com/GabrielReisR/CarreirasContemporaneas/master/Bancos/network%20analysis%20-%20especifico.csv", sep = ",")
     networkEsp <- networkEsp %>% select(-X)
-    # formando a matriz de correlações
+
+ # formando a matriz de correlações
      networkEsp <- as.matrix(networkEsp)
      networkCorEsp <- cor(networkEsp)
      sdsEsp <- sqrt(diag(networkCorEsp))
@@ -27,47 +29,49 @@
                            graph = "glasso", sampleSize = 300)
     networkGraphEsp # grafo de rede
 
- # Valores das relações dos itens
+ # Valores das correlações dos itens
     networkMagnitudesEsp <- getWmat(networkGraphEsp)
     networkMagnitudesEsp
     # Tabela das correlações dos itens
     plotMagnitudesEsp <- cor.plot(networkMagnitudesEsp, numbers = TRUE)
  
  # Medidas de centralidade
-    centrality1 <- centrality_auto(networkGraph1)
+    centralityEsp <- centrality_auto(networkGraphEsp)
 
-    nodeCentrality1 <- centralityItens_1$node.centrality
-    edge1 <- centralityItens_1$edge.betweenness.centrality
-    centralityPlot(networkGraph1, labels = colnames(network1))
+    nodeCentralityEsp <- centralityEsp$node.centrality
+    edgeEsp <- centralityEsp$edge.betweenness.centrality
+    centralityPlot(networkGraphEsp, labels = colnames(networkEsp))
 
-'''Criando rede 2 - escores gerais
-    AEO 
-    + ACSF
-    + ACP
-    + AUTEN + BALAN + CRESC
-    + EEC
-    '''
-    # removendo o que não entra na análise
-    network2 <- network %>% select(-X, -MP, -MF, -AG, -DV)
-    # formando a matriz de correlações
-     network1 <- as.matrix(network1)
-     networkCor1 <- cor(network1)
-     sds1 <- sqrt(diag(networkCor1))
-     networkMatrix1 <- cor2cov(networkCor1, sds1) # objeto final
+# Criando rede 2 - escores gerais
+ #   AEO 
+ #   + ACSF
+ #   + ACP
+ #   + AUTEN + BALAN + CRESC
+ #   + EEC
 
-    networkGraph1 <- qgraph(networkMatrix1, directed = FALSE, layout = "spring", 
+ # lendo banco específico
+    networkGeral <- read.csv("https://raw.githubusercontent.com/GabrielReisR/CarreirasContemporaneas/master/Bancos/network%20analysis%20-%20geral.csv", sep = ",")
+    networkGeral <- networkGeral %>% select(-X)
+
+ # formando a matriz de correlações
+     networkGeral <- as.matrix(networkGeral)
+     networkCorGeral <- cor(networkGeral)
+     sdsGeral <- sqrt(diag(networkCorGeral))
+     networkMatrixGeral <- cor2cov(networkCorGeral, sdsGeral) # objeto final
+
+    networkGraphGeral <- qgraph(networkMatrixGeral, directed = FALSE, layout = "spring", 
                            graph = "glasso", sampleSize = 300)
-    networkGraph1 # grafo de rede
+    networkGraphGeral # grafo de rede
 
  # Valores das relações dos itens
-    networkMagnitudes1 <- getWmat(networkGraph1)
-    networkMagnitudes1
+    networkMagnitudesGeral <- getWmat(networkGraphGeral)
+    networkMagnitudesGeral
     # Tabela das correlações dos itens
-    plotMagnitudes1 <- cor.plot(networkMagnitudes1, numbers = TRUE)
+    plotMagnitudesGeral <- cor.plot(networkMagnitudesGeral, numbers = TRUE)
  
  # Medidas de centralidade
-    centrality1 <- centrality_auto(networkGraph1)
+    centralityGeral <- centrality_auto(networkGraphGeral)
 
-    nodeCentrality1 <- centralityItens_1$node.centrality
-    edge1 <- centralityItens_1$edge.betweenness.centrality
-    centralityPlot(networkGraph1, labels = colnames(network1))
+    nodeCentralityGeral <- centralityGeral$node.centrality
+    edgeGeral <- centralityGeral$edge.betweenness.centrality
+    centralityPlot(networkGraphGeral, labels = colnames(networkGeral))
