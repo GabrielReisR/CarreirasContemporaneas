7924b7f549d8a3339d249289e7d94cf49fc5e2cc
Gabriel Reis <reisrgabriel@gmail.com>
2020-05-28 12:14:23 -0300

    Organização do projeto
    Banco linkado ao github, variáveis arrumadas.

--- Análises/Cleaning Dataset.R
+++ Análises/Cleaning Dataset.R
@@ -5,8 +5,9 @@
   library(tidyverse)
 
 #Entendendo o banco de dados
-  carreira <- read_csv("carreira.csv")
-  
+  banco_inicial <- read_csv("https://raw.githubusercontent.com/GabrielReisR/CarreirasContemporaneas/master/Bancos/bancosintegrados.csv")
+  View(banco_inicial) #visualização do banco
+
   #Nome das variáveis
     #AEO  = Autoeficácia Ocupacional
     #ACSF = Atitude de Carreira sem Fronteiras
@@ -15,17 +16,26 @@
     #EEC  = Engajamento com a Carreira
 
   #A variável ID parece estranha, é melhor olhá-la de perto
-    carreira <- carreira %>% arrange(ID)
-    carreira
+    banco_inicial <- banco_inicial %>% arrange(ID)
+    banco_inicial
 
-    #ID está significando "Concordo Discordo" ao invés de identificar os participantes
-    #X1 irá substituir ID (X1 -> id)
-      carreira <- carreira %>% rename(id = X1)
+    #ID está significando "Concordo/Discordo" ao invés de identificar os participantes
+    #Criando uma nova ID
+     id <- 1:300
+     banco_inicial <- cbind(banco_inicial, id)
+     glimpse(banco_inicial)
     #Pronto! Agora vou selecionar "id" (em minúsculo) e prosseguir
     
 #Selecionando apenas as sete variáveis principais, já convertendo para números
-  carreira <- carreira %>%
+  banco_inicial <- banco_inicial %>%
     select(id, Sexo, AEO01:EEC09)
 
 #Análises descritivas iniciais
-  skim(carreira)
+  skim(banco_inicial) #algumas respostas que deveriam ser inteiras estão fracionadas
+
+  #Transformando em números inteiros
+  banco_inicial <- round(banco_inicial)
+  skim(banco_inicial) #tudo ok!
+
+#Novo banco limpo - prosseguir para análises de carreira
+write.csv(banco_inicial, "carreira.csv")
